931ccd256d89538990a4080d9546dfc352d745cf
davidcoats <david@getsafetycone.com>
2022-11-16 09:35:42 -0800

    Initial commit.

---
+++ source/R5T.Q0004/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.Q0004
+{
+	/// <summary>
+	/// BlazorTemplater examinations (explorations, experiments, and demonstrations).
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.Q0004/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.Q0004
+BlazorTemplater examinations (explorations, experiments, and demonstrations).
